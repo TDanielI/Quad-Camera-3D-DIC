5f1270ec316f16299f3e2442fa81b1a12ab70efe
MaxxTG <70283216+MaxxTG@users.noreply.github.com>
2023-06-30 09:37:01 +0700

    Updated get_rectified_image.m
    Fixed bug where img2 did not have the correct affine transformation.

--- matlab/get_rectified_image.m
+++ matlab/get_rectified_image.m
@@ -8,7 +8,7 @@ function [tformed1, tformed2] = get_rectified_image(t, R, img1, img2)
 % Output
 % - tformed1 : First image with the affine mapping applied
 % - tformed2 : Second image with the affine mapping applied
-% 
+%%%
 
 % The normal vector of the joint plane is
 % z3 = t x (z1 x z2)
@@ -23,31 +23,40 @@ y3 = unitize(cross(z3, x3)); % unitize actually optional since by definition x3 
 % The above x3, y3, z3 are in the world coordinates
 % In frame 3's coordinates, they are [1; 0; 0], [0; 1; 0], and [0; 0; 1]
 % respectively.
-% Thus Rinv = [x3 y3 z3] transforms frame 3 vectors to world frame's vectors
-% Conversely, Ri = Rinv' = [x3'; y3'; z3'] transforms world frame's vectors to
+% Thus Rinv = [x3 y3 z3] transforms frame 3 vectors to frame 1's vectors
+% Conversely, Ri = Rinv' = [x3'; y3'; z3'] transforms frame 1's vectors to
 % frame 3 vectors
 % === 
-% The following explicitly states R, but we skip the computation and head
-% directly to the affine map A
-% Ri = zeros(3, 3);
-% Ri(1, :) = x3';
-% Ri(2, :) = y3';
-% Ri(3, :) = z3'; 
+Ri = zeros(3, 3);
+Ri(1, :) = x3';
+Ri(2, :) = y3';
+Ri(3, :) = z3'; 
+% Define Rj = Ri*R which transforms frame 2's vectors to frame 3 vectors
+Rj = Ri*R
 
 % Now we use
 % - x = 0 midway between frame 1 and frame 2's origin. i.e. at +t/2 in the
 % world coordinate
 % - y = 0 at the epipolar line
-% - z = 0 far enough to ensure all the images 
+% - z = 0 at the epipolar line
 
 % Using an orthography projection from 3D to 2D, our final map is affine
-% A = [Ri(1, 1), Ri(1, 2), Ri(1, 3) + vecnorm(t)/2; Ri(2, :); 0, 0, 1]
-A = zeros(3, 3)
-A(1, :) = x3' + [0, 0, (vecnorm(t)/2)]
-A(2, :) = y3'
-A(3, :) = [0, 0, 1] % Standard for affines
-tform = affinetform2d(A)
+% A1 = [Ri(1, 1), Ri(1, 2), Ri(1, 3) + vecnorm(t)/2; Ri(2, :); 0, 0, 1]
+A1 = zeros(3, 3)
+A1(1, :) = Ri(1, :) + [0, 0, (vecnorm(t)/2)]
+A1(2, :) = Ri(2, :)
+A1(3, :) = [0, 0, 1] % Standard for affines
+
+% A2 is similar, but
+% - Uses Rj instead of Ri
+% - Uses -vecnorm(t)/2 instead of +vecnorm(t)/2
+A2 = zeros(3, 3)
+A2(1, :) = Rj(1, :) - [0, 0, (vecnorm(t)/2)]
+A2(2, :) = Rj(2, :)
+A2(3, :) = [0, 0, 1]
 
 % Use the affine mapping to obtain the projected images
-tformed1 = imwarp(img1, tform)
-tformed2 = imwarp(img2, tform)
+tform1 = affinetform2d(A1)
+tform2 = affinetform2d(A2)
+tformed1 = imwarp(img1, tform1)
+tformed2 = imwarp(img2, tform2)
